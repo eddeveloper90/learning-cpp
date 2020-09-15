19808d0e9f789edfc3f1abad586e38711dd2a37c
eddeveloper90 <ed.developer90@gmail.com>
2020-09-16 01:50:35 +0430

    refactor

--- lesson01.cpp
+++ lesson01.cpp
@@ -2,10 +2,9 @@
  * email: ed.developer90@gmail.com
  * using auto
  * */
+#include <iostream>
 
 using namespace std;
-
-#include <iostream>
 int main() {
     auto n1 = 10;   // int n1
     auto n2 = 10.5; // double n2
