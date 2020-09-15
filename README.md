8f5d79e9ad5db1a4dc33f61620d38c7d209f837b
eddeveloper90 <ed.developer90@gmail.com>
2020-09-16 01:49:19 +0430

    README.md file improved.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 this is my cpp tutorial for who wants to start coding with.
 <br>
+
+I used "Modern C++ Programming CookBook :: @ Marius Bancila" as reference of my codes.
+
+<br>
 enjoy
---
+++ lesson01.cpp
@@ -0,0 +1,17 @@
+/**
+ * email: ed.developer90@gmail.com
+ * using auto
+ * */
+
+using namespace std;
+
+#include <iostream>
+int main() {
+    auto n1 = 10;   // int n1
+    auto n2 = 10.5; // double n2
+    auto text = "some text"; // char const *
+    auto list = {1, 2, 3}; // std:initializer_list<int>
+
+
+    return 0;
+}
